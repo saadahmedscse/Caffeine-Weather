e6eb3c0ca41b626d49a31414222781ea058d96c6
Saad Ahmed <saadahmedscse@gmail.com>
2022-04-19 05:37:20 +0600

    Initial Commit

---
+++ app/src/main/java/com/caffeine/caffeineweather/view/activity/FrontActivity.kt
@@ -0,0 +1,12 @@
+package com.caffeine.caffeineweather.view.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.caffeine.caffeineweather.R
+
+class FrontActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_front)
+    }
+}
